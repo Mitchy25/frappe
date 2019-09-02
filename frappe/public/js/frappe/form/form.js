b6e423dd57dc2d4f29227580ffdbb4d4e300fdc3
Mitch Stokes <Mitch@Fxmed.co.nz>
2019-09-02 12:27:30 +1200

    Update form.js

--- frappe/public/js/frappe/form/form.js
+++ frappe/public/js/frappe/form/form.js
@@ -850,7 +850,8 @@ frappe.ui.form.Form = class FrappeForm {
 		new frappe.views.CommunicationComposer({
 			doc: this.doc,
 			frm: this,
-			subject: __(this.meta.name) + ': ' + this.docname,
+			//subject: __(this.meta.name) + ': ' + this.docname,
+			subject:'',
 			recipients: this.doc.email || this.doc.email_id || this.doc.contact_email,
 			attach_document_print: false,
 			message: message,
