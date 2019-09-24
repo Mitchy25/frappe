4563b170d8e37e673f0ad391102eb6f7c80db29b
Mitch Stokes <Mitch@Fxmed.co.nz>
2019-09-24 16:14:27 +1200

    Update smtp.py

--- frappe/email/smtp.py
+++ frappe/email/smtp.py
@@ -61,7 +61,7 @@ def get_outgoing_email_account(raise_exception_not_set=True, append_to=None, sen
 			email_account = _get_email_account({
 				"enable_outgoing": 1,
 				"enable_incoming": 1,
-				#"append_to": append_to,
+				"append_to": append_to,
 				"email_id": sender_email_id
 			})
 
@@ -152,6 +152,7 @@ def get_default_outgoing_email_account(raise_exception_not_set=True):
 
 def _get_email_account(filters):
 	name = frappe.db.get_value("Email Account", filters)
+	frappe.log_error("Email returned: " + str(name))
 	return frappe.get_doc("Email Account", name) if name else None
 
 class SMTPServer:
