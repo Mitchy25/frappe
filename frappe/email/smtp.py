1dba462b809bf79e4cd0aaf860c9bd041d97b320
Mitch Stokes <Mitch@Fxmed.co.nz>
2019-09-24 16:29:31 +1200

    Update smtp.py

--- frappe/email/smtp.py
+++ frappe/email/smtp.py
@@ -98,7 +98,7 @@ def get_outgoing_email_account(raise_exception_not_set=True, append_to=None, sen
 			email_account.default_sender = email.utils.formataddr((email_account.name, email_account.get("email_id")))
 
 		frappe.local.outgoing_email_account[append_to or sender_email_id or "default"] = email_account
-		frappe.log_error("Testing: " + str(frappe.local.outgoing_email_account[append_to or sender_email_id or "default"]))
+		frappe.log_error("Testing: " + str(frappe.local.outgoing_email_account[append_to or sender_email_id or "default"].__dict__))
 	
 	return frappe.local.outgoing_email_account.get(append_to) \
 		or frappe.local.outgoing_email_account.get(sender_email_id) \
