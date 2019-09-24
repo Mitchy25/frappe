168ff6e1e7e9f75db4a5bb6a677725a7dfaa00b8
Mitch Stokes <Mitch@Fxmed.co.nz>
2019-09-24 17:30:58 +1200

    Update smtp.py

--- frappe/email/smtp.py
+++ frappe/email/smtp.py
@@ -84,8 +84,11 @@ def get_outgoing_email_account(raise_exception_not_set=True, append_to=None, sen
 					"append_to": append_to
 				})
 
+		frappe.log_error("Email Account:" + str(email_account))
+		frappe.log_error("Sender Email ID: "  + str(sender_email_id))
 		if not email_account and sender_email_id:
 			# check if the sender has email account with enable_outgoing
+			
 			email_account = _get_email_account({"enable_outgoing": 1, "email_id": sender_email_id})
 			frappe.log_error("SQL Result - Email Account: " + str(email_account.__dict__) + " Sender Email ID: " + str(sender_email_id))
 			
