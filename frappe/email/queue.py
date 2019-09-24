2ac76a3cd504c2fee636e0c9066113424482e6da
Mitch Stokes <Mitch@Fxmed.co.nz>
2019-09-24 14:40:23 +1200

    Update queue.py

--- frappe/email/queue.py
+++ frappe/email/queue.py
@@ -400,6 +400,7 @@ def send_one(email, smtpserver=None, auto_commit=True, now=False, from_test=Fals
 	try:
 		if not frappe.flags.in_test:
 			if not smtpserver: smtpserver = SMTPServer()
+			frappe.log_error("The sender is: " +str(email.sender))
 			smtpserver.setup_email_account(email.reference_doctype, sender=email.sender)
 
 		for recipient in recipients_list:
