95811670991ea49ea0bcc70ebbb1eef425e5c88e
Mitch Stokes <Mitch@Fxmed.co.nz>
2019-09-26 16:25:57 +1200

    Update upload.js

--- frappe/public/js/frappe/upload.js
+++ frappe/public/js/frappe/upload.js
@@ -1,7 +1,9 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // MIT License. See license.txt
 
-import FileUploader from './file_uploader/index.js';
+//import FileUploader from './file_uploader/index.js';
+
+const FileUploader = require('./file_uploader/index.js').default
 
 frappe.provide('frappe.ui');
 frappe.ui.FileUploader = FileUploader;
