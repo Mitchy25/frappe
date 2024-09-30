69327b59edc9f341bc36a7898b0ec5a67e1b9e78
Tom Fretwell <Tom@Fxmed.co.nz>
2024-10-01 10:10:17 +1300

    Fix: Removing button

--- frappe/contacts/doctype/contact/contact.js
+++ frappe/contacts/doctype/contact/contact.js
@@ -27,17 +27,17 @@ frappe.ui.form.on("Contact", {
 			frm.perm[0].write &&
 			frappe.boot.user.can_create.includes("User")
 		) {
-			frm.add_custom_button(__("Invite as User"), function () {
-				return frappe.call({
-					method: "frappe.contacts.doctype.contact.contact.invite_user",
-					args: {
-						contact: frm.doc.name,
-					},
-					callback: function (r) {
-						frm.set_value("user", r.message);
-					},
-				});
-			});
+			// frm.add_custom_button(__("Invite as User"), function () {
+			// 	return frappe.call({
+			// 		method: "frappe.contacts.doctype.contact.contact.invite_user",
+			// 		args: {
+			// 			contact: frm.doc.name,
+			// 		},
+			// 		callback: function (r) {
+			// 			frm.set_value("user", r.message);
+			// 		},
+			// 	});
+			// });
 		}
 		frm.set_query("link_doctype", "links", function () {
 			return {
