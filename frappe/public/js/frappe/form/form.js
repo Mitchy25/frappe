70425a355cef0d8f292a1ca560add7ca5e60e01e
Tom Fretwell <Tom@Fxmed.co.nz>
2024-10-07 15:31:37 +1300

    Fix; Adding dirty trigger logic back

--- frappe/public/js/frappe/form/form.js
+++ frappe/public/js/frappe/form/form.js
@@ -313,7 +313,7 @@ frappe.ui.form.Form = class FrappeForm {
 
 		// using $.each to preserve df via closure
 		$.each(table_fields, function (i, df) {
-			frappe.model.on(df.options, "*", function (fieldname, value, doc) {
+			frappe.model.on(df.options, "*", function (fieldname, value, doc, skip_dirty_trigger=false) {
 				if (doc.parent == me.docname && doc.parentfield === df.fieldname) {
 					if (!skip_dirty_trigger) {
 						me.dirty();
