1a036d513a1842fc0840822d08d35d29b64358bc
tom <tom@fxmed.co.nz>
2026-07-02 09:38:25 +1200

    Feat: Adding production_site flag to boot

--- frappe/boot.py
+++ frappe/boot.py
@@ -170,7 +170,7 @@ def load_conf_settings(bootinfo):
 	from frappe.core.api.file import get_max_file_size
 
 	bootinfo.max_file_size = get_max_file_size()
-	for key in ("developer_mode", "socketio_port", "file_watcher_port"):
+	for key in ("developer_mode", "socketio_port", "file_watcher_port", "production_site"):
 		if key in frappe.conf:
 			bootinfo[key] = frappe.conf.get(key)
 
